8974699b20ab9ec7bac6987d4adc11ef1d59688a
Sean King <sean@seanking.co>
2020-11-30 15:05:35 +1300

    Fix window check on outbound setting for SSR environments

--- src/Link.svelte
+++ src/Link.svelte
@@ -23,7 +23,7 @@
     if (response.ok) fetched.set(href, true);
   }
 
-  $: if (window && window.URL) {
+  $: if (typeof window !== 'undefined') {
     const isExternal =
       new URL(href, `${location.protocol}//${location.host}`).host !==
       location.host;
